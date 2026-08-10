cb2b322c3d87ba44a2e50dacbd32baa9d4ef0cab
01100101011001110110111101110010 <kolokolnikovea@gmail.com>
2026-08-10 16:42:51 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Homepage from './pages/Homepage.jsx';
+import Pricing from './pages/Pricing.jsx';
+import Product from './pages/Product.jsx';
+import PageNotFound from './pages/PageNotFound.jsx';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/pricing' element={<Pricing />} />
+        <Route path='/product' element={<Product />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import PageNav from '../components/PageNav';
+
+function Homepage() {
+  return (
+    <div>
+      <PageNav />
+      <h3>Home page</h3>
+    </div>
+  );
+}
+
+export default Homepage;
