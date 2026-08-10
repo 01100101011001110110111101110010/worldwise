4be85ee7a710679b0e81c92bcec46f92b651bc3a
01100101011001110110111101110010 <kolokolnikovea@gmail.com>
2026-08-10 19:36:50 +0700

    feat: add styled sidebar to app layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from './Footer.module.css';
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.copyright}>
+        {new Date().getFullYear()} &copy; WorldWise inc. All rights reserved.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,8 +1,12 @@
-import AppNav from '../components/AppNav.jsx';
+import SideBar from '../components/SideBar.jsx';
+import Map from '../components/Map.jsx';
+import styles from './AppLayout.module.css';
+
 function AppLayout() {
   return (
-    <div>
-      <AppNav />
+    <div className={styles.app}>
+      <SideBar />
+      <Map />
     </div>
   );
 }
